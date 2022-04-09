71e8bdff624a01385c8d69bd3c22698fc6510fda
Aizirek <aizirek.abdubaetova@gmail.com>
2022-04-09 18:12:39 +0200

    Hello World 2.2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,6 +11,7 @@ public class HelloWorld {
     //Doing something
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Wooden Spoon");
     }
 }
 
